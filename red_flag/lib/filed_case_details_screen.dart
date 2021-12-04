c459b3a4688df3703b111fc256d1ffba8d11367c
Siddhant Saxena <71447112+siddhantsaxena25@users.noreply.github.com>
2021-12-04 21:40:03 +0530

    Update filed_case_details_screen.dart

--- red_flag/lib/filed_case_details_screen.dart
+++ red_flag/lib/filed_case_details_screen.dart
@@ -8,7 +8,7 @@ import './dashboard_screen.dart';
 import './home_page_ongoing_cases.dart';
 
 class FiledCaseDetailsScreen extends StatefulWidget {
-  const FiledCaseDetailsScreen({ Key? key }) : super(key: key);
+  const FiledCaseDetailsScreen({Key? key}) : super(key: key);
 
   static const routeName = "./filed_case_details_screen";
 
@@ -20,7 +20,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: PreferredSize(
+      appBar: PreferredSize(
         preferredSize: const Size.fromHeight(66.0),
         child: AppBar(
           automaticallyImplyLeading: false,
@@ -68,7 +68,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                             ),
                           ),
                           Text(
-                            "Aadhar number",
+                            LoginScreen.aadhar,
                             style: GoogleFonts.poppins(
                               color: Colors.white,
                               fontSize: 18.0,
@@ -82,7 +82,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                             color: Colors.white,
                           ),
                           const SizedBox(
-                            height: 36.0,
+                            height: 32.0,
                           ),
                           Container(
                             width: double.infinity,
@@ -99,7 +99,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                                     "Ongoing Cases",
                                     style: GoogleFonts.poppins(
                                       color: Colors.white,
-                                      fontSize: 30.0,
+                                      fontSize: 24.0,
                                       fontWeight: FontWeight.w500,
                                     ),
                                   ),
@@ -113,7 +113,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                                     "File a case",
                                     style: GoogleFonts.poppins(
                                       color: Colors.white,
-                                      fontSize: 30.0,
+                                      fontSize: 24.0,
                                       fontWeight: FontWeight.w500,
                                     ),
                                   ),
@@ -127,7 +127,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                                     "Report a missing child",
                                     style: GoogleFonts.poppins(
                                       color: Colors.white,
-                                      fontSize: 30.0,
+                                      fontSize: 24.0,
                                       fontWeight: FontWeight.w500,
                                     ),
                                   ),
@@ -141,7 +141,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                                     "Dashboard",
                                     style: GoogleFonts.poppins(
                                       color: Colors.white,
-                                      fontSize: 30.0,
+                                      fontSize: 24.0,
                                       fontWeight: FontWeight.w500,
                                     ),
                                   ),
@@ -152,7 +152,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                                     "Help/FAQs",
                                     style: GoogleFonts.poppins(
                                       color: Colors.white,
-                                      fontSize: 30.0,
+                                      fontSize: 24.0,
                                       fontWeight: FontWeight.w500,
                                     ),
                                   ),
@@ -163,7 +163,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                                     "Settings",
                                     style: GoogleFonts.poppins(
                                       color: Colors.white,
-                                      fontSize: 30.0,
+                                      fontSize: 24.0,
                                       fontWeight: FontWeight.w500,
                                     ),
                                   ),
@@ -172,7 +172,7 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
                             ),
                           ),
                           const SizedBox(
-                            height: 36.0,
+                            height: 32.0,
                           ),
                           const Divider(
                             thickness: 4.0,
@@ -223,6 +223,291 @@ class _FiledCaseDetailsScreenState extends State<FiledCaseDetailsScreen> {
           backgroundColor: Colors.red,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Image.file(
+                      FileACaseScreen.image!,
+                      width: 114.0,
+                      height: 114.0,
+                    ),
+                    const SizedBox(width: 16.0),
+                    Column(
+                      children: [
+                        Text(
+                          "Case Number:",
+                          style: GoogleFonts.poppins(
+                            color: Colors.red,
+                            fontSize: 22.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          FileACaseScreen.caseNumber,
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 22.0,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 28.0,
+              ),
+              Container(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "State:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.state,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "District:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.district,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Date of disappearance:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.date,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Time of disappearance:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.time,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Place of disappearance:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.place,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Name:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.name,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Age:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.age,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Gender:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.gender,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Appearance:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.appearance,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Illnesses, if any:",
+                            style: GoogleFonts.poppins(
+                              color: Colors.red,
+                              fontSize: 22.0,
+                            ),
+                          ),
+                          Text(
+                            FileACaseScreen.illness,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushNamed(OngoingCasesScreen.routeName);
+                        },
+                        child: Text(
+                          "Return to home page",
+                          style: GoogleFonts.poppins(
+                            color: const Color(0xFF107C10),
+                            fontSize: 17.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
